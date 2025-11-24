4cd069d4b77f9dde93dd13c38e0d5edfea568ec2
Nevin <n.boyd1@yahoo.com>
2025-11-24 15:38:21 -0800

    Add User & Recipe models with validations and migrations

--- server/models.py
+++ server/models.py
@@ -1,21 +1,56 @@
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.hybrid import hybrid_property
 from marshmallow import Schema, fields
-
 from config import db, bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    # relationship: User has many Recipes
+    recipes = relationship("Recipe", backref="user")
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes are not readable.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.id}: {self.username}>"
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f"<Recipe {self.id}: {self.title}>"
 
 class UserSchema(Schema):
-    pass
+    id = fields.Int()
+    username = fields.Str()
+    image_url = fields.Str()
+    bio = fields.Str()
 
 class RecipeSchema(Schema):
-    pass
+    id = fields.Int()
+    title = fields.Str()
+    instructions = fields.Str()
+    minutes_to_complete = fields.Int()
+    user = fields.Nested(UserSchema)
